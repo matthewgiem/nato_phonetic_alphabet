6bda68b0c915e832cbeca68c08b8e7ebf02e4a9b
Matthew Giem <matthew.giem.programmer@gmail.com>
2023-05-25 18:36:33 +0700

    refactor code

--- main.py
+++ main.py
@@ -2,12 +2,8 @@ import pandas as pd
 
 df = pd.read_csv("nato_phonetic_alphabet.csv")
 
-alternative_nato_dictionary = {row.letter: row.code for (index, row) in df.iterrows()}
+nato_dictionary = {row.letter: row.code for (index, row) in df.iterrows()}
 
 word = input("what word would you like translated? ")
-return_list = []
-for x in word:
-    if x != " ":
-        return_list.append(alternative_nato_dictionary[x.upper()])
-
+return_list = [nato_dictionary[x.upper()] for x in word if x != " "]
 print(return_list)
